cdf54302cdf46551b412eb8a8c0121d285cf41d3
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-04 13:36:26 -0700

    Update DrawingSurface.java

--- src/DrawingSurface.java
+++ src/DrawingSurface.java
@@ -1,5 +1,16 @@
+import processing.core.PApplet;
 
-public class DrawingSurface {
+public class DrawingSurface extends PApplet{
 	public static final int DRAWING_WIDTH = 800;
 	public static final int DRAWING_HEIGHT = 600;
+	private World world;
+	
+	public DrawingSurface() {
+		super();
+		world = new World();
+	}
+	
+	public void draw() {
+		world.draw(this);
+	}
 }
