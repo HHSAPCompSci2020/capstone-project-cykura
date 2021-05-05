566249367499bde9e4493315cb2f6a78f9a7e433
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-04 21:33:52 -0700

    made hero a subclass

--- src/Enemy.java
+++ src/Enemy.java
@@ -5,14 +5,25 @@ import processing.core.PImage;
 
 public class Enemy extends MovingImage{
 	public int health;
+	private int v;
 
 	public Enemy(PImage img, int x, int y, int w, int h) {
 		super(img, x, y, w, h);
+		v=2;
 		
 	}
 	
 	public void move(Hero hero, ArrayList<Shape> obstacles) {
-		
+		 int MoveToX = hero.getX();
+	     int MoveToY = hero.getY();
+
+	     int diffX = MoveToX - x;
+	     int diffY = MoveToY - y;
+
+	     float angle = (float)Math.atan2(diffY, diffX);
+
+	     x += v * Math.cos(angle);
+	     y += v * Math.sin(angle);
 	}
 
 }
--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,10 @@
+import processing.core.PImage;
 
-public class Hero {
+public class Hero extends MovingImage{
+
+	public Hero(PImage img, int x, int y, int w, int h) {
+		super(img, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
 
 }
