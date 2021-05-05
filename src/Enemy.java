6624000286251c90a700190dd7a9608b5d6bdff6
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-04 21:10:19 -0700

    added movingimage

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,10 @@
+import processing.core.PImage;
 
-public class Enemy {
+public class Enemy extends MovingImage{
+
+	public Enemy(PImage img, int x, int y, int w, int h) {
+		super(img, x, y, w, h);
+		
+	}
 
 }
