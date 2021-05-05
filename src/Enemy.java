f0e2916e77a6e350b9cc273b979a08c7fe0ae3fa
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-04 21:45:10 -0700

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -5,15 +5,24 @@ import processing.core.PImage;
 
 public class Enemy extends MovingImage{
 	public int health;
-	
+	private int v;
 
 	public Enemy(PImage img, int x, int y, int w, int h) {
 		super(img, x, y, w, h);
-		
+		v = 2;
 	}
 	
 	public void move(Hero hero, ArrayList<Shape> obstacles) {
-		
+		 double x1 = hero.x;
+	     double y1 = hero.y;
+
+	     double diffX = x1 - x;
+	     double diffY = y1 - y;
+
+	     float angle = (float)Math.atan2(diffY, diffX);
+
+	     x += v * Math.cos(angle);
+	     y += v * Math.sin(angle);
 	}
 
 }
