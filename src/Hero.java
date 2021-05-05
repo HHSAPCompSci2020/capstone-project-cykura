d932a634450882cd588fdfc5f0c8885e46861d8a
TheVic23 <vvijayakumar471@student.fuhsd.org>
2021-05-04 21:39:14 -0700

    Start on Hero

--- src/Hero.java
+++ src/Hero.java
@@ -6,7 +6,8 @@ import processing.core.PImage;
  * Date Modified: 5.4.21
  */
 public class Hero extends MovingImage{
-
+	int
+	
 	public Hero(PImage img, int x, int y, int w, int h) {
 		super(img, x, y, w, h);
 	}
