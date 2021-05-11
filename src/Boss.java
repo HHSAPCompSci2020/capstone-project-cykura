0762c69ec7469acdeccbfe172c57d242e7b5e655
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-10 19:33:06 -0700

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -11,6 +11,7 @@ public class Boss extends Enemy {
 	private int invertTime;
 	public ArrayList<Fireball> fireballs;
 	public WaterWave w;
+	private int cooldown;
 	/**
 	 * Creates a new instance of a Boss object having its left
 	 * corner at the inputed (x, y) coordinates.
@@ -24,7 +25,15 @@ public class Boss extends Enemy {
 		fireballs = new ArrayList<Fireball>();
 	}
 	
-	public void act() {
+	public void act(Hero hero) {
+		double x1 = hero.x;
+	    double y1 = hero.y;
+
+	    double diffX = x1 - x;
+	    double diffY = y1 - y;
+	    
+	    float angle = (float)Math.atan2(diffY, diffX);
+	    //Inverting Screen
 		if(Math.random()>0.993&&GameScreen.invertControls==false) {
 			invertControls((int)Math.random()*5000+3000);
 		}
@@ -35,6 +44,20 @@ public class Boss extends Enemy {
 			invertTime = 0;
 			GameScreen.invertControls = false;
 		}
+		//Shooting Fireballs
+		if(Math.random()>0.99&& w==null) {
+			Fireball f = new Fireball(GameScreen.fireball, (int)x, (int)y,20,20, v*Math.cos(angle)*2, v*Math.sin(angle)*2);
+			fireballs.add(f);
+			cooldown=60;
+		}
+		if(cooldown>0) {
+			cooldown--;
+		}
+		//Use WaterWave
+		if(Math.random()>0.99&&cooldown<=0) {
+			w = new WaterWave((int)x,(int)y,100,100,5);
+		}
+		
 	}
 
 	private void invertControls(int duration) {
