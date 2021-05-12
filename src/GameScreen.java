9ce6d6907083e3f51fd27f49ad07722c392d5f9f
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-11 20:42:22 -0700

    Update GameScreen.java

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -229,10 +229,12 @@ public class GameScreen extends Screen {
 		float t_b = view_y+Vertical_Margin;
 		if(hero.y<t_b) {
 			view_y-=t_b-hero.y;
+			System.out.println("vertical");
 		}
 		float h_b = view_y+DRAWING_HEIGHT-Horizontal_Margin;
 		if(hero.y+hero.height>h_b) {
-			view_y+=h_b+hero.y;
+			System.out.println("horizontal");
+			view_y+=h_b-hero.y;
 		}
 		surface.translate(-view_x,-view_y);
 	}
