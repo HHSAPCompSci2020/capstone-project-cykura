22f4583a7989952a88d32f0a060a31f7bac317c9
Animan Patil <56457146+apatil987@users.noreply.github.com>
2021-05-12 08:19:24 -0700

    testing fireenemy

--- src/FireEnemy.java
+++ src/FireEnemy.java
@@ -41,7 +41,7 @@ public class FireEnemy extends Enemy{
 		   	     waitTime=45;
 		    }
 		    if(Math.random()>0.985) {
-		    	Fireball f = new Fireball(GameScreen.fireball, (int)x, (int)y,20,20, v*Math.cos(angle)*2, v*Math.sin(angle)*2);
+		    	Fireball f = new Fireball(GameScreen.fireball, (int)x, (int)y,20,20, v*Math.cos(angle)*3, v*Math.sin(angle)*3);
 		    	fireballs.add(f);
 		    }
 		     if(waitTime<=0) {
