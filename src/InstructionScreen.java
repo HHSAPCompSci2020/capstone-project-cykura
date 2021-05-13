5134c8d6cb9cd614e419f687f524a06e037ceb19
TheVic23 <vvijayakumar471@student.fuhsd.org>
2021-05-12 22:48:39 -0700

    OptionMenuWorks

--- src/InstructionScreen.java
+++ src/InstructionScreen.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.awt.Rectangle;
 
 /**
@@ -49,25 +50,17 @@ public class InstructionScreen extends Screen {
 		surface.fill(0);
 		String str1 = "Option Menu";
 		float w1 = surface.textWidth(str1);
-		surface.text(str1, firstScreenButton.x+firstScreenButton.width/2-w1/2, (firstScreenButton.y+firstScreenButton.height/2) +10);
-
-		
-//		surface.rect(gameButton.x, gameButton.y, gameButton.width, gameButton.height, 10, 10, 10, 10);
-//		surface.fill(0);
-//		String str = "Cykura";
-//		float w = surface.textWidth(str);
-//		surface.text(str, gameButton.x+gameButton.width/2-w/2, gameButton.y+gameButton.height/2);
-//		
-//		surface.fill(255);
-//		surface.rect(instructionsButton.x, instructionsButton.y, instructionsButton.width, instructionsButton.height, 10, 10, 10, 10);
-//		surface.fill(0);
-//		String str1 = "Instructions";
-//		float w1 = surface.textWidth(str1);
-//		surface.text(str1, instructionsButton.x+instructionsButton.width/2-w1/2, instructionsButton.y+instructionsButton.height/2);
-		
+		surface.text(str1, firstScreenButton.x+firstScreenButton.width/2-w1/2, (firstScreenButton.y+firstScreenButton.height/2) +10);		
 		
 		surface.popStyle();
 
+	}
+	
+	public void mousePressed() {
+		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
+		if (firstScreenButton.contains(p))
+			surface.switchScreen(ScreenSwitcher.SCREEN1);
+				
 	}
 
 }
