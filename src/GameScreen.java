6f7c69b1c85da94ac75888acb22c179ceb06d9f6
TheVic23 <vvijayakumar471@student.fuhsd.org>
2021-05-22 09:52:32 -0700

    Added second moving platform

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -410,7 +410,7 @@ public class GameScreen extends Screen {
 		p.add(new Rectangle(5725, 235,75,50));
 		p.add(new Rectangle(5400, 235,75,50));
 		//Parkour4
-		p.add(new Rectangle(6100,345,800,50));
+//		p.add(new Rectangle(6100,345,800,50));	// temp
 		//Final Boss
 		p.add(new Rectangle(7100,345,800,50));	
 		p.add(new Rectangle(7450,245,100,100));
@@ -465,6 +465,8 @@ public class GameScreen extends Screen {
 		ArrayList<MovingPlatform> m=new ArrayList<MovingPlatform>();
 //		m.add(new MovingPlatform(movingPlatform,200,345,100,50,400,345,2,0));
 		m.add(new MovingPlatform(movingPlatform, 4100,345,200,50, 5100,345,2,0)); //temp
+		m.add(new MovingPlatform(movingPlatform, 5900, 345, 200, 50, 6900, 345, 2, 0));	// temp
+
 		return m;
 	}
 	/**
